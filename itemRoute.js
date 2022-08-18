c983aab96b633bba7f3bf7a6e63224e2245fdbbc
Edmond Soun <edmond.soun@gmail.com>
2022-08-17 17:04:04 -0700

    work on patch route

--- itemRoute.js
+++ itemRoute.js
@@ -12,33 +12,42 @@ const router = new express.Router();
 
 ///ROUTES 
 
-router.get('/', function(req,res) {
+router.get('/', function (req, res) {
 
-    return res.status(200).json({ items: items});
+    return res.status(200).json({ items: items });
 
 });
 
-router.post('/', function(req,res) {
+router.post('/', function (req, res) {
 
     items.push(req.body);
-    return res.status(201).json({added: items.at(-1)});
+    return res.status(201).json({ added: items.at(-1) });
 
 });
 
-router.get('/:name', function(req,res) {
+router.get('/:name', function (req, res) {
+    const name = req.params.name;
+    const singleItem = items.find(item => item.name === name);
+
+    return res.status(200).json(singleItem);
+});
+
+router.patch('/:name', function (req, res) {
+
     debugger
-    const name = req.params.name
-    const singleItem = items.find(item => item.name === name)
+    
+    const name = req.params.name;
+    let singleItem = items.find(item => item.name === name);
 
-    return res.status(200).json(singleItem)
+    const index = items.indexOf(singleItem);
+
+    items[index] = req.body;
+
+    return res.status(200).json(items[index]);
+
 });
 
-router.patch('/:name', function(req,res) {
-
-
-});
-
-router.delete('/:name', function(req,res) {
+router.delete('/:name', function (req, res) {
 
 
 });
